47578fd64b252a399eab3648f86ed4b76c5d6c85
c-victorino <jcd.victorino@gmail.com>
2023-07-15 01:44:42 -0400

    fixed issues with Promises

--- blog-service.js
+++ blog-service.js
@@ -35,114 +35,132 @@ Post.belongsTo(Category, { foreignKey: "category" });
 // ensure that we can connect to the DB and that our Post and
 // Category models are represented in the database as tables
 function initialize() {
-  return sequelize
-    .sync()
-    .then(() => resolve())
-    .catch((err) => reject("unable to sync the database"));
+  return new Promise((resolve, reject) => {
+    sequelize
+      .sync()
+      .then(() => resolve())
+      .catch((err) => reject("unable to sync the database"));
+  });
 }
 
 // retrieves all posts from the PostgreSQL database
 function getAllPosts() {
-  return sequelize.sync().then(() => {
-    Post.findAll()
-      .then((posts) => resolve(posts))
-      .catch((err) => reject("no results returned"));
+  return new Promise((resolve, reject) => {
+    sequelize.sync().then(() => {
+      Post.findAll()
+        .then((posts) => resolve(posts))
+        .catch((err) => reject("no results returned"));
+    });
   });
 }
 
 // gets all post who's published val is set to true
 function getPublishedPosts() {
-  return sequelize.sync().then(() => {
-    Post.findAll({
-      where: { published: true },
-    })
-      .then((data) => resolve(data))
-      .catch((err) => reject("unable to create post"));
+  return new Promise((resolve, reject) => {
+    sequelize.sync().then(() => {
+      Post.findAll({
+        where: { published: true },
+      })
+        .then((data) => resolve(data))
+        .catch((err) => reject("unable to create post"));
+    });
   });
 }
 
 // Retrieves all categories from the PostgreSQL database
 function getCategories() {
-  return sequelize.sync().then(() => {
-    Category.findAll()
-      .then((data) => resolve(data))
-      .catch((err) => reject("no results returned"));
+  return new Promise((resolve, reject) => {
+    sequelize.sync().then(() => {
+      Category.findAll()
+        .then((data) => resolve(data))
+        .catch((err) => reject("no results returned"));
+    });
   });
 }
 
 // get post who's category value is the value passed to the function
 function getPostsByCategory(category) {
-  return sequelize.sync().then(() => {
-    Post.findAll({
-      where: {
-        category,
-      },
-    })
-      .then((posts) => resolve(posts))
-      .catch((err) => reject("no results returned"));
+  return new Promise((resolve, reject) => {
+    sequelize.sync().then(() => {
+      Post.findAll({
+        where: {
+          category,
+        },
+      })
+        .then((posts) => resolve(posts))
+        .catch((err) => reject("no results returned"));
+    });
   });
 }
 
 // get post that contains postDate value greater than or equal to the minDateStr
 function getPostsByMinDate(minDateStr) {
-  return sequelize.sync().then(() => {
-    const { gte } = Sequelize.Op;
-    Post.findAll({
-      where: {
-        postDate: {
-          [gte]: new Date(minDateStr),
+  return new Promise((resolve, reject) => {
+    sequelize.sync().then(() => {
+      const { gte } = Sequelize.Op;
+      Post.findAll({
+        where: {
+          postDate: {
+            [gte]: new Date(minDateStr),
+          },
         },
-      },
-    })
-      .then((data) => resolve(data))
-      .catch((err) => reject("no results returned"));
+      })
+        .then((data) => resolve(data))
+        .catch((err) => reject("no results returned"));
+    });
   });
 }
 
 // get post who's id value is the value passed to the function
 function getPostById(id) {
-  return sequelize.sync().then(() => {
-    Post.findAll({
-      where: { id },
-    })
-      .then((data) => resolve(data[0]))
-      .catch((err) => reject("no results returned"));
+  return new Promise((resolve, reject) => {
+    sequelize.sync().then(() => {
+      Post.findAll({
+        where: { id },
+      })
+        .then((data) => resolve(data[0]))
+        .catch((err) => reject("no results returned"));
+    });
   });
 }
 
 // filter the results by "published" & "category"
 // (using the value true for "published" & the value passed to the function
 function getPublishedPostsByCategory(category) {
-  return sequelize.sync().then(() => {
-    Post.findAll({
-      where: {
-        published: true,
-        category: category,
-      },
-    })
-      .then((data) => resolve(data))
-      .catch((err) => reject("no results returned"));
+  return new Promise((resolve, reject) => {
+    sequelize.sync().then(() => {
+      Post.findAll({
+        where: {
+          published: true,
+          category: category,
+        },
+      })
+        .then((data) => resolve(data))
+        .catch((err) => reject("no results returned"));
+    });
   });
 }
 
 // saves the postData to a PostgreSQL database
 function addPost(postData) {
-  return sequelize.sync().then(() => {
-    // Ensure value is correct when user toggled or not the
-    // published property in the form
-    postData.published = postData.published ? true : false;
-    for (let key in postData) {
-      // make any blank values be null when user did not input a value on a field
-      if (postData[key] === "") {
-        postData[key] = null;
+  return new Promise((resolve, reject) => {
+    sequelize.sync().then(() => {
+      // Ensure value is correct when user toggled or not the
+      // published property in the form
+      postData.published = postData.published ? true : false;
+      for (let key in postData) {
+        // make any blank values be null when user did not input a value on a field
+        if (postData[key] === "") {
+          postData[key] = null;
+        }
       }
-    }
-    // assign postDate val as current date when posted
-    postData.postDate = new Date();
+      // assign postDate val as current date when posted
+      postData.postDate = new Date();
 
-    Post.create(postData)
-      .then((data) => resolve(data))
-      .catch((err) => "unable to create post");
+      Post.create(postData)
+        .then((data) => resolve(data))
+        .catch((err) => "unable to create post");
+    });
   });
 }
 
